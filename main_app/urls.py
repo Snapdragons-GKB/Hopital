bf3bf339ed6c452b4e0ead89943480be9d5b0773
diakt <igs2107@columbia.edu>
2022-08-29 15:58:56 -0400

    cleaned urls, views

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,44 +2,35 @@ from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 
-
-#create routes for patient portal, maps to views.py
 urlpatterns=[
-    #initial definitions for portal
+    #General patterns
+    #If template, found in general-templates folder
     path('', views.Nothing, name='nothing'), #A redirect to the landing page
-    path('landing/', views.Landingpage, name='home'), #The landing page for everyone
+    path('landing/', views.Landingpage, name='landing'), #The landing page
+    path('signup/', views.Signup.as_view(), name='signup'), #The signup page
+    path('login/', views.Login.as_view(), name='login'), #The login page
+    path('logout/', views.Logout.as_view(), name='logout'), #The logout page
+    path('about/', views.About, name='about'), #The about page
+
+
+    #Patient patterns
+    #If template, found in patient-templates folder
+    path('patient/patient-registration/', views.Patient_Additional_Reg.as_view(), name="patient-registration"),
+    path('patient/home/', views.Patient_Home, name="patient-home"),
+    path('patient/request/', views.Patient_Request_Appointment.as_view(), name="patient-request"),
+    path('patient/detail/', views.Patient_Details, name="patient-detail"),
+
+    #Provider patterns
+    #If template, found in provider-templates folder
+    path('provider/provider-registration/', views.Provider_Additional_Reg.as_view(), name="provider-registration"),
+    path('provider/home/', views.Provider_Home, name="provider-home"),
+    path('provider/detail/', views.Provider_Details, name="provider-detail"),
     
-    #holy trinity of actions, defined in views.py
-    path('signup/', views.Signup.as_view(), name="signup"),
-    path('login/', views.Login.as_view(), name="login"),
-    path('logout/', views.Logout.as_view(), name="logout"),
-
-
-    #routes for additional patient/provider registration
-    path('signup/patient-registration/', views.Patient_Additional_Reg.as_view(), name="patient-registration"),
-    path('signup/provider-registration/', views.Provider_Additional_Reg.as_view(), name="provider-registration"),
-
-    #somewhat diffusely defined behaviors for before/after login, treat with some skepticism
-    path('home/', views.Home, name='home'), 
-    path('welcome/', views.Welcome, name="welcome"),
-    path('about/', views.About, name="about"),
     
+    #Scheduler patterns
+    #If template, found in scheduler-templates folder
+    path('scheduler/home/', views.Scheduler_Home, name="scheduler-home"),
 
-    #route for testing patient request
-    path('patient/request/', views.Patient_Request_Appointment.as_view(), name='patient_request'),
-    path('patient/detail/', views.Patient_Details, name='patient_detail'), 
-
-
-
-#create routes for Scheduler/Admin portal
-    # path('admin/', views.Home_Admin, name='admin'),
-    # path('admin/patient_request/', views.Admin_Patient_Request.as_view(), name='admin_patient_request'),
-    # path('admin/patient_list/', views.Admin_Patient_list.as_view(), name='admin_patient_list'),
-
-#create routes for Provider portal
-    path('provider/', views.Provider_Home, name='provider'),
-    # path('provider/schedule', views.Provider_Schedule, name='schedule'),
-    # path('provider/client_list', views.Provider_Clientlist, name='client_list')
 
 
     
--- main_app/views.py
+++ main_app/views.py
@@ -5,119 +5,33 @@ from main_app.forms import AdditionalPatient, AdditionalProvider, UserForm, Pati
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, logout, get_user_model, login
 
-from main_app.models import Scheduler, Patient
+from main_app.models import Scheduler, Patient, Provider
 User=get_user_model()
 
 # Create your views here.
 
 
+#General views
+#If template, found in general-templates folder
+
 def Nothing(request):
     return redirect('landing/')
 
 def Landingpage(request): 
-    return render(request, 'landingpage.html')
-
-@login_required(redirect_field_name=None)
-
-# Views for patient portal 
-def Home(request):
-    #return render(request, '../main_app/templates/home.html')
-    return render(request, 'home.html')
-
-def Welcome(request):
-    return render(request, 'welcome.html')
+    return render(request, 'general-templates/landingpage.html')
 
 def About(request):
-    return render(request, 'about.html')
+    return render(request, 'general-templates/about.html')
 
-def Patient_Details(request):
-    #got our patient data by calling model
-    patientdata = Patient.objects.all()
-    #got our current user's id
-    ouruserrightnowID = request.user.id
-    print(request.user.id)
-    #iterated through all patients in our data to find a patient with our current user's id
-    for patient in patientdata:
-        print(patient)
-        if patient.patientProfile_id == ouruserrightnowID:
-            #returned a dictionary with our 
-            return render(request, 'details.html', {
-                "age":patient.patient_age, 
-                "insurance": patient.patient_insurance_type,
-                "conditions": patient.patient_preexisting_conditions,
-                "medications": patient.patient_current_medications
-                })
-
-    return render(request, 'details.html', frozenset('a'))
-    
-    # return render(request, 'details.html', recastdata)
-
-# views for Provider portal 
-def Provider_Home(request): 
-    return render(request, 'provider_base.html')
-
-# def Provider_Schedule(request): 
-#     return render(request, 'provider_schedule.html')
-
-# def Provider_Clientlist(request): 
-#     return render(request, 'provider_clientlist')
-
-
-
-
-
-# def login(request) :
-#     if request.method == 'POST':
-#         form = login(request.POST)
-#         if form.is_valid():
-#           username = form.cleaned_data['email']
-#           password = form.cleaned_data['password']
-#           user = authenticate(username=username, password=password)
-
-#           if user is not None:
-#               form = login()
-#               login(request, user)
-
-
-class Login(View):
-    def get(self, request):
-        form = UserLogin()
-        return render(request, 'registration/login.html', {'form': form})
-
-    def post(self, request):
-        form = UserLogin(data=request.POST)
-        user = authenticate(username=request.POST['username'], password=request.POST['password'])
-        if user is not None:
-            print("glorious")
-            login(request, user)
-            return redirect('home')
-        # if user.check_password(request.POST['password']):
-        #     login(request, user)
-        #     return redirect('home')
-        # print(user)
-        else:
-            print("fail")
-            return render(request, 'registration/login.html', {'form': form})
-
-       
-        
-
-
-
-
-
-
-
-
-#classic signup form. On post, signs user in and then checks their usertype, routing to appropriate additionals or home page
 class Signup(View): 
     def get(self, request):
         form = UserForm()
         context = {"form": form}
-        return render(request, "registration/signup.html", context)
+        return render(request, "general-templates/signup.html", context)
 
     def post(self, request):
         form = UserForm(data=request.POST)
+        context = {"form": form}
         if form.is_valid():
             user = form.save()
             #user.set_password(user.password) the true height of my insanity
@@ -125,90 +39,89 @@ class Signup(View):
             login(request, user)
 
             if user.usertype == 'Patient':
-                # return render(request, "additional_reg/patient-reg.html", {"form": form})
                 return redirect('patient-registration/')
             elif user.usertype == 'Scheduler':
                 sched = Scheduler()
                 sched.schedulerProfile=request.user
-                sched.save() #this may work, check later
-                return render(request, "home.html")
+                sched.save()
+                return render(request, "scheduler-templates/scheduler-home.html")
             elif user.usertype == 'Provider':
                 return redirect('provider-registration/')
         else:
-            return render(request, "registration/signup.html", {"form": form})
+            return render(request, "registration/signup.html", context)
 
-#The next two methods tender additional details for patient and provider registration.
-class Patient_Additional_Reg(View):
+class Login(View):
     def get(self, request):
-        form = AdditionalPatient()
-        context = {"form": form}
-        return render(request, "additional-reg/patient-reg.html", context)
+        form = UserLogin()
+        return render(request, 'general-templates/login.html', {'form': form})
 
     def post(self, request):
-        form = AdditionalPatient(request.POST)
-        if form.is_valid():
-            patient = form.save(commit=False)
-            patient.patientProfile = request.user
-            patient.save()
-            return render(request, "home.html")
-
+        form = UserLogin(data=request.POST)
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            login(request, user)
+            return redirect('home')
         else:
-            return render(request, "additional-reg/patient-reg.html", {"form": form})
+            return render(request, 'general-templates/login.html', {'form': form})
 
-
-
-
-
-class Provider_Additional_Reg(View): 
-    def get(self, request):
-        form = AdditionalProvider()
-        context = {"form": form}
-        return render(request, "additional-reg/provider-reg.html", context)
-
-    def post(self, request):
-        form = AdditionalProvider(request.POST)
-        if form.is_valid():
-            provider = form.save(commit=False)
-            provider.providerProfile =request.user
-            provider.save()
-            return render(request, "provider_base.html")
-
-        else:
-            return render(request, "additional-reg/provider-reg.html", {"form": form})
-
-
-
-
-
-
-
-
-#classic login form. On post, checks usertype, routing to separate home page, though that is not built out yet
-
-
-
-
-
-
-
-
-    
 class Logout(View):
     def get(self, request):
         user=request.user
         if user is not None:
             logout(request)
-            return render(request, "landingpage.html")
+            return render(request, "general-templates/landingpage.html")
 
 
-#Here be dragons
-###############################################################################################################################
+
+
+#Patient views
+#If template, found in patient-templates folder
+
+def Patient_Home(request):
+    return render(request, 'patient-templates/patient-home.html')
+
+def Patient_Welcome(request):
+    return render(request, 'patient-templates/patient-welcome.html')
+
+def Patient_Details(request):
+    patientdata = Patient.objects.all()
+    ouruserrightnowID = request.user.id
+    for patient in patientdata:
+        print(patient)
+        if patient.patientProfile_id == ouruserrightnowID:
+            return render(request, 'patient-templates/patient-details.html', {
+                "age":patient.patient_age, 
+                "insurance": patient.patient_insurance_type,
+                "conditions": patient.patient_preexisting_conditions,
+                "medications": patient.patient_current_medications
+                })
+
+    return render(request, 'details.html', frozenset('a'))
+
+class Patient_Additional_Reg(View):
+    def get(self, request):
+        form = AdditionalPatient()
+        context = {"form": form}
+        return render(request, "patient-templates/additional-patient-registration.html", context)
+
+    def post(self, request):
+        form = AdditionalPatient(request.POST)
+        context = {"form": form}
+        if form.is_valid():
+            patient = form.save(commit=False)
+            patient.patientProfile = request.user
+            patient.save()
+            return render(request, "patient-templates/patient-home.html")
+
+        else:
+            return render(request, "patient-templates/additional-patient-registration.html", context)
+
 
 class Patient_Request_Appointment(View):
     def get(self, request):
         form = PatientRequestForAppointment()
         context = {"form": form}
-        return render(request, "patient/patient-request.html", context)
+        return render(request, "patient-templates/patient-request.html", context)
         
     def post(self, request):
         form = PatientRequestForAppointment(request.POST)
@@ -218,4 +131,60 @@ class Patient_Request_Appointment(View):
             patientrequest.save()
             return render(request, "welcome.html")
         else:
-            return render(request, "patient/patient-request.html", {"form": form})
+            return render(request, "patient-templates/patient-request.html", {"form": form})
+
+
+
+
+#Provider views
+#If template, found in provider-templates folder
+
+def Provider_Home(request): 
+    return render(request, 'provider-templates/provider-home.html')
+
+def Provider_Welcome(request):
+    return render(request, 'provider-templates/provider-welcome.html')
+
+def Provider_Details(request):
+    providerdata = Provider.objects.all()
+    ouruserrightnowID = request.user.id
+    for provider in providerdata:
+        print(provider)
+        if provider.providerProfile_id == ouruserrightnowID:
+            return render(request, 'provider-templates/provider-details.html', {
+                "blurb": provider.provider_blurb,
+                "specialization": provider.provider_specialization,
+                "insurances": provider.provider_insurances_taken,
+                })
+
+    return render(request, 'details.html', frozenset('a'))
+
+class Provider_Additional_Reg(View): 
+    def get(self, request):
+        form = AdditionalProvider()
+        context = {"form": form}
+        return render(request, "provider-templates/additional-provider-registration.html", context)
+
+    def post(self, request):
+        form = AdditionalProvider(request.POST)
+        context = {"form": form}
+        if form.is_valid():
+            provider = form.save(commit=False)
+            provider.providerProfile =request.user
+            provider.save()
+            return render(request, "provider-templates/provider-home.html")
+
+        else:
+            return render(request, "provider-templates/additional-provider-registration.html", context)
+
+
+
+
+#Scheduler views
+#If template, found in scheduler-templates folder
+
+def Scheduler_Home(request):
+    return render(request, 'scheduler-templates/scheduler-home.html')
+
+def Scheduler_Welcome(request):
+    return render(request, 'scheduler-templates/scheduler-welcome.html')
