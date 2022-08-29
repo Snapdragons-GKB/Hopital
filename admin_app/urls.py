1d6f256bd16572558da5d86629b00691f6329f2b
diakt <igs2107@columbia.edu>
2022-08-29 04:31:33 -0400

    yachtzee

--- admin_app/urls.py
+++ admin_app/urls.py
@@ -20,7 +20,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path routed to look for all uls in main_app
-    path('accounts/', include('django.contrib.auth.urls')),
+    #path('accounts/', include('django.contrib.auth.urls')),
     path('', include('main_app.urls')),
 
 ]
